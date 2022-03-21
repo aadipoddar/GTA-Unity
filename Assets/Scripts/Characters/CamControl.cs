f0851b809e05706a465e4a2513725682fa751199
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-21 15:04:59 +0530

    Camera Control

---
+++ Assets/Scripts/Characters/CamControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    public int rotateSpeed = 200;
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse X") < 0)
+        {
+            transform.Rotate(0, -rotateSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+    }
+}
