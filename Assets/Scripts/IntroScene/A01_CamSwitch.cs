3caabcf5fea773b581308afadc73493b898044cb
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-22 00:20:29 +0530

    Skybox and UI

--- Assets/Scripts/IntroScene/A01_CamSwitch.cs
+++ Assets/Scripts/IntroScene/A01_CamSwitch.cs
@@ -6,6 +6,7 @@ public class A01_CamSwitch : MonoBehaviour
 {
     public GameObject firstCam;
     public GameObject secondCam;
+    public GameObject credLeadDes;
 
     void Start()
     {
@@ -14,7 +15,9 @@ public class A01_CamSwitch : MonoBehaviour
 
     IEnumerator CamSwitcher()
     {
-        yield return new WaitForSeconds(7);
+        yield return new WaitForSeconds(3);
+        credLeadDes.SetActive(true);
+        yield return new WaitForSeconds(4);
         secondCam.SetActive(true);
         firstCam.SetActive(false);
     }
