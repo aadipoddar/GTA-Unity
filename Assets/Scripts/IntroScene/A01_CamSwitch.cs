7f5718d790d8ab89c2ff5a77d5d7fbbcc86ddf70
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-21 18:21:55 +0530

    CAMERA SWITCH

---
+++ Assets/Scripts/IntroScene/A01_CamSwitch.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A01_CamSwitch : MonoBehaviour
+{
+    public GameObject firstCam;
+    public GameObject secondCam;
+
+    void Start()
+    {
+        StartCoroutine(CamSwitcher());
+    }
+
+    IEnumerator CamSwitcher()
+    {
+        yield return new WaitForSeconds(7);
+        secondCam.SetActive(true);
+        firstCam.SetActive(false);
+    }
+}
