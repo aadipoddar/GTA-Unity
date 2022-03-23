f7b8f347b0ab239e70d56aac246ae2c3f3b552c5
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-23 22:11:16 +0530

    ACTIVE SFX and UI

--- Assets/Scripts/Characters/CharControl.cs
+++ Assets/Scripts/Characters/CharControl.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class CharControl : MonoBehaviour
 {
     public GameObject thePlayer;
+
+    public AudioSource footStep1;
+    public AudioSource footStep2;
+
     public bool isRunning;
+    public bool isStepping = false;
+
     public float horizontalMove;
     public float verticalMove;
+
+    public int stepNum;
 
     void Update()
     {
@@ -17,6 +25,8 @@ public class CharControl : MonoBehaviour
             horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
             verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 8;
             isRunning = true;
+            if (isStepping == false)
+                StartCoroutine(RunSound());
             transform.Rotate(0, horizontalMove, 0);
             transform.Translate(0, 0, verticalMove);
         }
@@ -26,5 +36,21 @@ public class CharControl : MonoBehaviour
             thePlayer.GetComponent<Animation>().Play("Idle");
             isRunning = false;
         }
+    }
+
+    IEnumerator RunSound()
+    {
+        if (isRunning == true && isStepping == false)
+        {
+            isStepping = true;
+            stepNum = Random.Range(1, 3);
+            if (stepNum == 1)
+                footStep1.Play();
+            else
+                footStep2.Play();
+        }
+
+        yield return new WaitForSeconds(0.3f);
+        isStepping = false;
     }
 }
