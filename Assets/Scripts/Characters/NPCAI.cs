144dae0bbd2ce7439fea5caf4989848bf0417162
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-26 17:14:34 +0530

    NPC AI

---
+++ Assets/Scripts/Characters/NPCAI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAI : MonoBehaviour
+{
+    public GameObject destinationPoint;
+    NavMeshAgent theAgent;
+
+    void Start()
+    {
+        theAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        theAgent.SetDestination(destinationPoint.transform.position);
+    }
+}
