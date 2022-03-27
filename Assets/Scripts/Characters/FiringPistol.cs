a54a36810cc5a4d66db0e48b52a58453f55850e8
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-27 17:27:04 +0530

    FLEEING NPC

--- Assets/Scripts/Characters/FiringPistol.cs
+++ Assets/Scripts/Characters/FiringPistol.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class FiringPistol : MonoBehaviour
 {
     public bool isAiming = false;
-    public GameObject thePlayer;
-    public AudioSource pistolShot;
     public static bool isFiring = false;
-    
+
+    public GameObject thePlayer;
+    public GameObject aimingObject;
+
+    public AudioSource pistolShot;
+
     void Update()
     {
         if (Input.GetMouseButton(1))
         {
             isAiming = true;
             if (isFiring == false)
+            {
                 thePlayer.GetComponent<Animation>().Play("Aiming1Pistol");
+                aimingObject.SetActive(true);
+            }
         }
         else
+        {
             isAiming = false;
+            aimingObject.SetActive(false);
+        }
 
         if (isAiming == true && Input.GetMouseButtonDown(0))
         {
---
+++ Assets/Scripts/Characters/NPC/NPCAlert.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAlert : MonoBehaviour
+{
+    void OnTriggerEnter(Collider npc)
+    {
+        if (npc.tag == "NPC")
+        {
+            npc.gameObject.GetComponent<Animator>().Play("Running");
+            npc.GetComponent<NavMeshAgent>().speed = 7.5f;
+        }
+    }
+}
